f9c4f065dcb11c18305e8be4e76e9fa90dbe2e51
beakyman64 <glasscot14@gmail.com>
2018-04-13 16:02:18 +0100

    tags spaced and moved

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -47,16 +47,7 @@ if ( ! function_exists( 'straped_posted_by' ) ) :
 		);
 
 		echo '<span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
-
-	}
-endif;
-
-if ( ! function_exists( 'straped_entry_footer' ) ) :
-	/**
-	 * Prints HTML with meta information for the categories, tags and comments.
-	 */
-	function straped_entry_footer() {
-		// Hide category and tag text for pages.
+		
 		if ( 'post' === get_post_type() ) {
 			/* translators: used between list items, there is a space after the comma */
 			$categories_list = get_the_category_list( esc_html__( ', ', 'straped' ) );
@@ -66,14 +57,10 @@ if ( ! function_exists( 'straped_entry_footer' ) ) :
 			}
 
 			/* translators: used between list items, there is a space after the comma */
-			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'straped' ) );
-			if ( $tags_list ) {
-				/* translators: 1: list of tags. */
-				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'straped' ) . '</span>', $tags_list ); // WPCS: XSS OK.
-			}
+			
 		}
-
-		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+		
+if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
 			echo '<span class="comments-link">';
 			comments_popup_link(
 				sprintf(
@@ -90,7 +77,21 @@ if ( ! function_exists( 'straped_entry_footer' ) ) :
 				)
 			);
 			echo '</span>';
-		}
+		}		
+
+	}
+endif;
+
+if ( ! function_exists( 'straped_entry_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function straped_entry_footer() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+			}
+
+		
 
 		edit_post_link(
 			sprintf(
