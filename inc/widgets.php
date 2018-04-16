0c7a1619689109510c298a85a87d60136dc15f57
beakyman64 <glasscot14@gmail.com>
2018-04-16 14:48:10 +0100

    add bootstrap panel to widget area

--- inc/widgets.php
+++ inc/widgets.php
@@ -9,13 +9,14 @@ function straped_widgets_init() {
 		'name'          => esc_html__( 'Sidebar', 'straped' ),
 		'id'            => 'sidebar-1',
 		'description'   => esc_html__( 'Add widgets here.', 'straped' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'before_widget' => '<section id="%1$s" class="panel panel-success widget %2$s">',
 		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
+		'before_title'  => '<div class="panel-heading"><h3 class="panel-title widget-title">',
+		'after_title'   => '</h3></div>',
 	) );
 }
 add_action( 'widgets_init', 'straped_widgets_init' );
 
 
 add_action( 'wp_enqueue_scripts', 'straped_scripts' );
+
